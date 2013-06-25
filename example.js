0c0da78a6124cd6064b3f571d4cf7a489c7488a9
rogerano <rogerano@gmail.com>
2013-06-25 12:22:57 +1000

    updated example

--- example.js
+++ example.js
@@ -4,9 +4,9 @@ topic.sub("topicname", function(one, two, three, four)
 {
   console.log("topicname executed with ", arguments, "or ", one, two, three, four);
 });
-var anotherHandler = topic.sub("anothertopic", function(argshere)
+var anotherHandler = topic.sub("topicname", function()
 {
-  console.log("anothertopic executed with ", arguments);
+  console.log("another topicname callback which should not execute.");
 });
 topic.unsub(anotherHandler);
 
